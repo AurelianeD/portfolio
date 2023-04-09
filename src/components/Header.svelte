427728522d3faf455b9e3904043d49c24c4b7488
Auréliane DOR <aureliane.dor@gmail.com>
2023-04-09 18:40:39 +0200

    feat: responsive header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
 	import {contacts} from "$lib/data/contact.ts";
-
-	let src ="src/lib/images/velonimo.png";
+	import type {Contact} from "$lib/data/contact.ts";
+	let src ="src/lib/images/aureliane.png";
 </script>
 
 
-<div class="p-6 rounded-b-2xl border-black border-2 md:max-w-[20%] self-start">
-	<div class="flex flex-row pb-10 md:flex-col">
-		<div class="w-[100px] border-2 border-black rounded-full overflow-hidden h-[100px] md:w-[100px]">
-			<img {src} class="object-contain"/>
+<div
+	class="
+		px-6 py-5 rounded-b-2xl border-black border-2 fixed z-10 bg-white
+		md:max-w-[20%] md:h-screen md:py-[5%] md:flex md:flex-col md:justify-between
+">
+	<div class="flex flex-row justify-between items-center pb-10 md:pb-0 md:flex-col">
+		<div class="border-2 border-black overflow-hidden h-20 w-20 rounded-full md:h-40 md:w-40">
+			<img {src} class="object-cover" alt="photo de profil">
 		</div>
-		<p class="min-w-[60%] my-0 mx-auto md:">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+		<p class="w-[70%] md:self-start md:w-full md:pt-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 	</div>
-	<div class="flex justify-evenly">
+	<ul class=" flex gap-6 justify-around underline md:flex-col">
+		<li>
+			<a href="#projects">Mes projets</a>
+		</li>
+		<li>
+			<a href="#about">A propos</a>
+		</li>
+	</ul>
+	<div class="hidden md:block">
+		<p class="pb-[10%]">Restons en contact :</p>
+		<div class="md:flex md:justify-evenly">
 		{#each contacts as contact}
 			<a href={contact.link}>
-				<img src={contact.icon} alt="" class="max-w-[50px]">
+				<img src={contact.icon} alt="icon réseaux sociaux" class="max-w-[50px]">
 			</a>
 		{/each}
+		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,15 @@
 
 <div class="flex flex-col md:flex-row">
 	<Header />
-	<div class="max-w-[80%]">
-		<h1>Projets</h1>
+	<div class="max-w-[80%] mx-auto md:mx-0 md:ml-[22%] mt-[60%] md:mt-10">
+		<h1 id="projects">Projets</h1>
 
 		<div class="flex flex-col gap-6 md:flex-row md:flex-wrap">
 			{#each projects as project}
 				<Card title={project.title} description={project.description} src={project.src} tags="{project.tags}" cardColor={project.cardColor}/>
 			{/each}
 		</div>
+		<h1 id="about">A propos</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet, aperiam deleniti dicta dignissimos distinctio error fuga id libero perspiciatis porro praesentium quaerat quis quo recusandae repellat sed, unde voluptatum.</p>
 	</div>
 </div>
