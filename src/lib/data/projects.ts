60148089613bae89b1c0e1ad8fa0adfe468c2f1c
Auréliane DOR <aureliane.dor@gmail.com>
2023-04-21 19:28:18 +0200

    add CV and new icons

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -3,7 +3,6 @@ export type Projects = {
 	description: string;
 	src: string;
 	tags: string[];
-	cardColor: string;
 	link: string;
 };
 
@@ -14,32 +13,28 @@ export const projects: Projects[] = [
 			'Application web React permettant de compter les points au fur et à mesure d’une partie de Vélonimo.',
 		src: 'src/lib/images/velonimo.png',
 		tags: ['React', 'TailwindCSS'],
-		cardColor: '#bf4194',
 		link: 'https://velonimo.aureliane.dev/'
 	},
 	{
 		title: 'Space Tourism',
-		description: 'Projet frontend Mentor, site responsive en Svelte.',
+		description: 'Projet Frontend Mentor, site static responsive en Svelte.',
 		src: 'src/lib/images/space.png',
 		tags: ['Svelte', 'TailwindCSS'],
-		cardColor: '#bf4194',
 		link: 'https://space-tourisme.aureliane.dev/'
 	},
 	{
 		title: 'Mon panier MIC',
-		description: 'Site web Prestashop pour la vente de produits des producteurs locaux.',
+		description: 'Site e-commerce Prestashop destiné la vente des produits des producteurs locaux.',
 		src: 'src/lib/images/mic.png',
 		tags: ['Prestashop'],
-		cardColor: '#bf4194',
 		link: 'https://monpanier-mic.com/index.php'
 	},
 	{
 		title: 'Embrouille au Musée - Projet MMI',
 		description:
-			'Expérience interactive sous forme d’une application web en partenariat avec le musée des Beaux Arts de Bordeaux.',
+			"Expérience interactive sous forme d’une application web en partenariat avec le musée des Beaux Arts de Bordeaux à l'occasion de la Bachanight 2023.",
 		src: 'src/lib/images/embrouille.png',
 		tags: ['Svelte Kit', 'TailwindCSS', 'Typescript'],
-		cardColor: '#bf4194',
 		link: 'https://embrouille-au-musee.netlify.app/'
 	},
 	{
@@ -47,16 +42,14 @@ export const projects: Projects[] = [
 		description: 'Jeux vidéo platformer en 2D, développé en C# avec Unity.',
 		src: 'src/lib/images/painter.png',
 		tags: ['Unity', 'C#'],
-		cardColor: '#bf4194',
 		link: 'https://la-machine-a-cafe.itch.io/the-mournful-painter'
 	},
 	{
 		title: 'Safe and Tech - Projet MMI',
 		description:
-			'Solution web responsive pour s’informer et se tester autrement sur l’hygiène informatique (quiz, classement par catégories des bonnes pratiques et leurs risques associé).',
+			'Solution web responsive pour s’informer et se tester autrement sur l’hygiène informatique.',
 		src: 'src/lib/images/safentech.png',
 		tags: ['Hugo', 'TailwindCSS'],
-		cardColor: '#bf4194',
 		link: 'https://safe-n-tech.netlify.app/'
 	}
 ];
