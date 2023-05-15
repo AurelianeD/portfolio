0ab311c8ffbf0a60129e2359bf4bfa5a7d0c4eaa
Auréliane DOR <aureliane.dor@gmail.com>
2023-05-15 18:02:03 +0200

    style laboratory page

--- src/routes/laboratory/+layout.svelte
+++ src/routes/laboratory/+layout.svelte
@@ -1,5 +1,19 @@
-<script></script>
+<script>
+	import { onMount, onDestroy } from 'svelte';
 
-<div class="p-10 bg-black h-screen">
+	onMount(() => {
+		const body = document.querySelector('body');
+		body.style.backgroundColor = 'black';
+	});
+	onDestroy(() => {
+		const body = document.querySelector('body');
+		body.style.backgroundColor = 'white';
+	});
+</script>
+
+<div class="p-10">
 	<slot />
 </div>
+
+<style>
+</style>
