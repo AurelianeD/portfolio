349a6bf1aa25937cb64ef19c3a01bb98c2d8a6a8
aurelianed <aureliane.dor@gmail.com>
2024-04-22 21:17:20 +0200

    add animation on cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import Tags from '$lib/components/Tags.svelte';
+	import {tweened} from "svelte/motion";
+	import {slide} from "svelte/transition"
+	import {cubicOut} from "svelte/easing";
 
 	export let title: string;
 	export let description: string;
 	export let src: string;
 	export let tags: string[];
+
+	const shadow = tweened(0, { duration: 500, easing: cubicOut });
+
+	function handleMouseEnter(){
+		shadow.set(1)
+	}
+	function handleMouseLeave(){
+		shadow.set(0)
+	}
 
 	let containerStyle =
 		'max-w-[400px] w-full h-full bg-white rounded-2xl flex flex-col overflow-hidden border-2 border-black ' +
@@ -12,9 +24,19 @@
 	let topCardStyle = ' relative border-b-2 border-black overflow-hidden';
 	let bottomCardStyle = 'py-4';
 	let textStyle = 'max-w-[80%] mx-auto';
+
 </script>
 
-<article class={containerStyle}>
+<article
+		class={containerStyle}
+		on:mouseenter={handleMouseEnter}
+		on:mouseleave={handleMouseLeave}
+		on:touchstart={handleMouseEnter}
+		on:touchend={handleMouseLeave}
+		transition:slide={{delay: 300}}
+		style="box-shadow: {$shadow * 6}px {$shadow * 3}px #191919, {$shadow * 12}px {$shadow * 6}px #191919, {$shadow *
+		18}px {$shadow * 9}px #191919; transform: translate(0, -{$shadow * 10}px "
+>
 	<div class={topCardStyle}>
 		<img {src} class="max-h-[150px] object-cover w-full" alt={title} />
 		<Tags {tags} />
